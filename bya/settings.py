bbefe069acc7a18b225b741d4a74587be348502f
Andy Doan <doanac@beadoan.com>
2017-01-03 23:41:53 -0600

    Allow local_settings support

--- bya/settings.py
+++ bya/settings.py
@@ -2,22 +2,13 @@ import logging
 import os
 
 _here = os.path.realpath(os.path.dirname(__file__))
+DATA_DIR = os.path.join(_here, '../../data/')
 
 WORKER_SCRIPT = os.path.join(_here, '../bya_worker.py')
 RUNNER_SCRIPT = os.path.join(_here, '../bya_runner.py')
 
 DEBUG = os.environ.get('DEBUG', '0')
 DEBUG = bool(int(DEBUG))
-if DEBUG:
-    SECRET_KEY = 'UNSAFE FOR PRODUCTION'
-else:
-    try:
-        with open(os.path.join(_here, '../../flask_secret')) as f:
-            SECRET_KEY = f.read()
-    except:
-        SECRET_KEY = os.urandom(24)
-        with open(os.path.join(_here, '../../flask_secret'), 'wb') as f:
-            f.write(SECRET_KEY)
 
 # used by notifications.py
 SERVER_NAME = 'localhost'
@@ -37,15 +28,27 @@ for fname in _settings_files:
         with open(fname) as f:
             exec(f.read())
 
-JOBS_DIR = os.path.join(_here, '../../job-defs')
-BUILDS_DIR = os.path.join(_here, '../../builds')
-QUEUE_DIR = os.path.join(_here, '../../run-queue')
-RUNNING_DIR = os.path.join(_here, '../../active-runs')
-HOSTS_DIR = os.path.join(_here, '../../hosts')
-TRIGGERS_DIR = os.path.join(_here, '../../triggers')
+JOBS_DIR = os.path.join(DATA_DIR, 'job-defs')
+BUILDS_DIR = os.path.join(DATA_DIR, 'builds')
+QUEUE_DIR = os.path.join(DATA_DIR, 'run-queue')
+RUNNING_DIR = os.path.join(DATA_DIR, 'active-runs')
+HOSTS_DIR = os.path.join(DATA_DIR, 'hosts')
+TRIGGERS_DIR = os.path.join(DATA_DIR, 'triggers')
 
 SECRETS_FILE = os.path.join(_here, '../../secrets.yml')
 
+if DEBUG:
+    SECRET_KEY = 'UNSAFE FOR PRODUCTION'
+else:
+    try:
+        with open(os.path.join(DATA_DIR, 'flask_secret')) as f:
+            SECRET_KEY = f.read()
+    except:
+        if not os.path.exists(DATA_DIR):
+            os.mkdir(DATA_DIR)
+        SECRET_KEY = os.urandom(24)
+        with open(os.path.join(DATA_DIR, 'flask_secret'), 'wb') as f:
+            f.write(SECRET_KEY)
 
 logging.bya_initialized = False
 
